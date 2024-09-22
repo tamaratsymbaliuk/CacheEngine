26fc773b5679bc0cfb1e4f871c0d2f0fa533682c
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-22 10:41:16 -0500

    CacheEngine - Interface defining methods for implementing cache behavior.

---
+++ src/main/java/Interfaces/ICache.java
@@ -0,0 +1,14 @@
+package Interfaces;
+/**
+ * Interface defining methods for implementing cache behavior.
+ */
+public interface ICache {
+    void put(String key, Integer value);
+    int get(String key);
+    void remove(String key);
+    // optional methods to implement
+    void clear();
+    int getSize();
+    boolean containsKey(String key);
+}
+
