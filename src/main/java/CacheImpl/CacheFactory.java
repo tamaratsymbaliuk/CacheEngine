58d1e5aa4939e8ed3df30d7e0d3f6fa80728acf6
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-22 10:40:27 -0500

    CacheEngine - CacheFactory class

--- src/main/java/CacheImpl/CacheFactory.java
+++ src/main/java/CacheImpl/CacheFactory.java
@@ -1,4 +1,38 @@
 package CacheImpl;
 
+import Enums.CacheTypeEnum;
+import Interfaces.ICache;
+
+import java.text.MessageFormat;
+
+/**
+ * Factory class for creating cache instances based on the specified cache type.
+ */
+
 public class CacheFactory {
+    /**
+     * Creates an instance of a cache based on the specified type and capacity.
+     *
+     * @param type     The type of cache to create (FIFO, LFU, LRU).
+     * @param capacity The maximum capacity of the cache.
+     * @return An instance of the specified cache type.
+     */
+    public static ICache createCacheInstance(CacheTypeEnum type, int capacity) {
+        switch (type) {
+            case FIFO -> {
+                return new FIFOCache(capacity);
+            }
+            case LFU -> {
+                LFUCacheBuilder lfuCacheBuilder = new LFUCacheBuilder().setCapacity(capacity).setServerName("localhost:8080");
+                return lfuCacheBuilder.build();
+            }
+            case LRU -> {
+                return new LRUCache(capacity);
+            }
+            default -> {
+                System.out.println(MessageFormat.format("Cache with type {0} is not implemented yet.", type));
+                return null;
+            }
+        }
+    }
 }
