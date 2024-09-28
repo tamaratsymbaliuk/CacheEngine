20fc8b8686f2720655188ddf20740721c6ba8627
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-28 12:00:00 -0500

    Main

--- src/main/java/CacheImpl/Main.java
+++ src/main/java/CacheImpl/Main.java
@@ -27,7 +27,7 @@ public class Main {
         //testLRUCache();
         //testFIFOMeasureTimeCacheDecorator();
         //testLegacyCacheAdapter();
-        testFIFOCacheIterator();
+        //testFIFOCacheIterator();
         testGenerics();
     }
 
