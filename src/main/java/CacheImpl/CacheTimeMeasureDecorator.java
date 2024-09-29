7abdd2763fceeae602b0c4456565554057cdd94f
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-29 14:31:55 -0500

    CacheEngine - A decorator for caching that measures the time taken for retrieval operations

--- src/main/java/CacheImpl/CacheTimeMeasureDecorator.java
+++ src/main/java/CacheImpl/CacheTimeMeasureDecorator.java
@@ -8,15 +8,15 @@ import java.text.MessageFormat;
  * A decorator for caching that measures the time taken for retrieval operations.
  */
 
-public class CacheTimeMeasureDecorator extends CacheDecorator {
-    public CacheTimeMeasureDecorator(ICache cache) {
+public class CacheTimeMeasureDecorator<K,V> extends CacheDecorator<K,V> {
+    public CacheTimeMeasureDecorator(ICache<K,V> cache) {
         super(cache);
     }
 
     @Override
-    public int get(String key) {
+    public V get(K key) {
         long startTime = System.nanoTime();
-        int cacheValue = super.get(key);
+        V cacheValue = super.get(key);
         long endTime = System.nanoTime();
         System.out.println(MessageFormat.format("Retrieved value for key: {0} in {1} ms", key, (endTime - startTime) / 1000000.0));
         return cacheValue;
